f82d7a0a9e6cfb2ca32b52f2ad076bce2ddaca34
fedocmac <fe.docmac@duocuc.cl>
2024-09-01 00:13:09 -0400

    registro añadido

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Auth } from '@angular/fire/auth';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { Auth, signOut } from '@angular/fire/auth';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,6 +9,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  authMode: string = 'login';
   email: string = '';
   password: string = '';
 
@@ -26,6 +27,28 @@ export class LoginPage implements OnInit {
     } catch (error) {
       console.error('Error al iniciar sesión:', error);
       alert('Email o contraseña incorrectos');
+    }
+  }
+
+  async register() {
+    try {
+      const user = await createUserWithEmailAndPassword(this.auth, this.email, this.password);
+      console.log('Registration successful', user);
+      this.router.navigateByUrl('/tabs/tab1'); // Redirige al usuario a la página principal después de registrarse
+    } catch (error) {
+      console.error('Error al registrarse:', error);
+      alert('Error al crear la cuenta. Por favor, intenta nuevamente.');
+    }
+  }
+
+  async logout() {
+    try {
+      await signOut(this.auth);
+      console.log('Logout successful');
+      this.router.navigateByUrl('/login'); // Redirige al usuario a la página de login
+    } catch (error) {
+      console.error('Error al cerrar sesión:', error);
+      alert('Error al cerrar sesión. Por favor, intenta nuevamente.');
     }
   }
 }
