7152d8a4134236874f14beba842fb0df704b95d4
maricons <107135463+maricons@users.noreply.github.com>
2024-09-09 23:54:55 -0300

    avance semana 6
    TAB 4 AGREGADO Y FUNCIONANDO :100:  :fire:

---
+++ src/app/tabs/tab4/tab4.module.ts
@@ -0,0 +1,21 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab4Page } from './tab4.page';
+import { ExploreContainerComponentModule } from '../../components/explore-container/explore-container.module';
+
+import { Tab4PageRoutingModule } from './tab4-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    Tab4PageRoutingModule,
+    ExploreContainerComponentModule
+  ],
+  declarations: [Tab4Page]
+})
+export class Tab4PageModule {}
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
         loadChildren: () => import('./tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'tab4',
+        loadChildren: () => import('./tab4/tab4.module').then(m => m.Tab4PageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
@@ -35,5 +39,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class TabsPageRoutingModule { }
